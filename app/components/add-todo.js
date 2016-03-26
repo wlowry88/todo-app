2b4032cdb301011128fff1ef5df04cc34c25fa38
Will Lowry <william.c.lowry@gmail.com>
2016-03-26 13:35:34 -0700

    add new todo-item

---
+++ app/components/add-todo.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+	actions: {
+		submit() {
+			const text = this.get('text');
+			this.get('onAdd')(text);
+			this.set('text', '');
+			this.$('input').focus();
+		}
+	}
+});
